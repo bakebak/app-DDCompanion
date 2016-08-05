ef8a8e9223f5be6b47112dc54c34cff0153ee71a
Luisa <luisa.rocha@digitaldesk.com.br>
2016-08-05 11:17:25 -0300

    Salvando dados do usuario

--- DDCompanion/www/scripts/script.js
+++ DDCompanion/www/scripts/script.js
@@ -2,27 +2,36 @@
     var self = this;
     self.showLogar = ko.observable(true);
     self.showDeslogar = ko.observable(false);
+    self.showDeslogarApp = ko.observable(false);
     self.userName = ko.observable("visitante");
+    self.usuario = ko.observable('');
 
     setTimeout(function () {
         var GooglePlus = window.plugins.googleplus;
-        alert("Iniciado");
+        var appp = plugins.appPreferences;
+        var appp = plugins.appPreferences;
+        appp.fetch(function (value) {
+            checarCampos(value);
+        }, function (err) {
+            alert("Erro " + err);
+        }, "usuario"
+        );
     }, 1000);
 
-    /*self.isAvailable = function () {
-        console.log("availb");
-        window.plugins.googleplus.isAvailable(function (avail) {
-            alert(avail);
-            console.log("Ativo");
-        });
-    }*/
+    function checarCampos(value) {
+        if (value != null) {
+            self.showDeslogarApp(true);
+            self.showDeslogar(false);
+            self.showLogar(false);
+            self.userName(value);
+        }
+    }
 
     self.login = function () {
         var GooglePlus = window.plugins.googleplus;
-        console.log("login");
+
         window.plugins.googleplus.login(
             {
-                //'scopes': 'profile email',
                 'scopes': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
                 'webClientId': "489399558653-rde58r2h6o8tnaddho7lathv2o135l7m.apps.googleusercontent.com",
                 'offline': true,
@@ -30,58 +39,45 @@
             function (result) {
                 self.showDeslogar(true);
                 self.showLogar(false);
-                console.log("obj");
-                console.log("Imagem" + result.imageUrl);
-                console.log("Nome" + result.displayName);
-                console.log("Email" + result.email);
-                console.log("idToken " + result.idToken);
-                console.log("serverAuthCode " + result.serverAuthCode);
-                console.log("oauthToken " + result.oauthToken);
-                console.log("accessToken " + result.accessToken);
-                console.log("refreshToken " + result.refreshToken);
-                self.userName (result.displayName);
-                //document.querySelector("#image").src = obj.imageUrl;
-                //document.querySelector("#image").style.visibility = 'visible';
-                // document.querySelector("#feedback").innerHTML = "Hi, " + obj.displayName + ", " + obj.email;
+                self.userName(result.displayName);
+                salvarDados(result);
             },
             function (msg) {
-                console.log("error msg");
-                //document.querySelector("#feedback").innerHTML = "error: " + msg;
+                alert("error msg");
             }
         );
     }
-
-    /*self.logout = function () {
-        var GooglePlus = window.plugins.googleplus;
-        console.log("logout");
-        window.plugins.googleplus.logout(
-        );
-    }*/
-
 
     self.disconnect = function () {
         var GooglePlus = window.plugins.googleplus;
         window.plugins.googleplus.disconnect(
         function (result) {
-            self.showDeslogar(false);
-            self.showLogar(true);
-            self.userName("visitante");
+            desconectar();
         }, function (msg) {
-            console.log("error desc");
-        }
-
-        );
+            alert("error desc");
+        });
     }
 
-    /*function trySilentLogin() {
-        window.plugins.googleplus.trySilentLogin(
-            {},
-            function (obj) {
-                alert("Ja logado");
-            },
-            function (msg) {
-                alert("error ja logado");
-            }
-        );
-    }*/
+    self.desconectarApp = function () {
+        var appp = plugins.appPreferences;
+        self.showDeslogar(false);
+        self.showDeslogarApp(false);
+        self.showLogar(true);
+        self.userName("visitante");
+
+        appp.remove(function (value) {
+            alert("Apagado" + value);
+        }, function (err) {
+            alert("Erro" + err);
+        }, "usuario");
+    }
+
+    function salvarDados(result) {
+        var appp = plugins.appPreferences;
+        appp.store(function (value) {
+            console.log("OK  " + value);
+        }, function (err) {
+            console.log("Erro " + err);
+        }, "usuario", result.displayName);
+    }
 }
