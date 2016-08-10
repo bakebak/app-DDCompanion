78ebc76b171e6a2890456f920a8f790b71441d90
Luisa <luisa.rocha@digitaldesk.com.br>
2016-08-10 16:53:05 -0300

    adição fontawesome, adição botão, requisições e salvamento do token

--- DDCompanion/www/scripts/script.js
+++ DDCompanion/www/scripts/script.js
@@ -1,11 +1,11 @@
 ﻿function ViewModel() {
     var self = this;
-        self.pagina = ko.observable('login');
-        self.userName = ko.observable("visitante");
-        self.usuario = ko.observable('');
-        self.botaoDesconectar = ko.observable(false);
-        self.botaoRemoverDados = ko.observable(false);
-        var token;
+    self.pagina = ko.observable('login');
+    self.userName = ko.observable("visitante");
+    self.usuario = ko.observable('');
+    self.botaoDesconectar = ko.observable(false);
+    self.token = ko.observable('..');
+    self.status = ko.observable('');
 
     setTimeout(function () {
         var GooglePlus = window.plugins.googleplus;
@@ -17,25 +17,22 @@
             console.log("Erro " + err);
         }, "usuario"
         );
+
+        appp.fetch(function (valueToken) {
+            self.token(valueToken);
+        }, function (err) {
+            console.log("Erro " + err);
+        }, "token"
+        );
+
     }, 500);
 
     function checarCampos(value) {
         if (value != null) { //já conectado
             self.pagina('home');
             self.userName(value);
-            self.botaoRemoverDados(true);
-            self.botaoDesconectar(false);
+            self.botaoDesconectar(true);
         }
-    }
-    self.disconnect = function () {
-        var GooglePlus = window.plugins.googleplus;
-        trySilentLogin();
-        window.plugins.googleplus.disconnect(
-        function (result) {
-            removerDados();
-        }, function (msg) {
-            alert(msg);
-        });
     }
     function trySilentLogin() {
         var GooglePlus = window.plugins.googleplus;
@@ -46,26 +43,50 @@
                 'offline': true,
             },
             function (obj) {
-                console.log(obj);
+                console.log("Logou");
             });
     }
-   function removerDados (){
+
+    self.disconnect = function () {
+        var GooglePlus = window.plugins.googleplus;
+        trySilentLogin();
+        window.plugins.googleplus.disconnect(
+        function (result) {
+            removerDados();
+        }, function (msg) {
+            alert("Disc " + msg);
+        });
+    }
+
+    function removerDados() {
         var appp = plugins.appPreferences;
         appp.remove(function (value) {
             self.pagina('login');
         }, function (err) {
             alert("Erro" + err);
         }, "usuario");
-   }
 
-   function salvarUsuario(result) {
+        appp.remove(function (valueToken) {
+        }, function (err) {
+            alert("Erro" + err);
+        }, "token");
+    }
+
+    function salvarUsuario(result) {
         var appp = plugins.appPreferences;
         appp.store(function (value) {
             console.log("Dados salvos:  " + value);
         }, function (err) {
             console.log("Erro " + err);
         }, "usuario", result.displayName);
+
+        appp.store(function (valueToken) {
+            console.log("Token salvo: " + valueToken);
+        }, function (err) {
+            console.log("Erro " + err);
+        }, "token", self.token());
     }
+
     self.login = function () {
         var GooglePlus = window.plugins.googleplus;
         window.plugins.googleplus.login(
@@ -76,45 +97,46 @@
             },
             function (result) {
                 var url = 'http://porta.digitaldesk.com.br/autenticar?token=' + result.serverAuthCode;
-                token = validarEmail(url);
-                emailValido(result);
-                //if (retornoEmail == true) {
-                //emailValido(result);
-                //}
-            },
-            function (msg) {
-                alert("Erro de conexão");
+                validarEmail(url, result);
             }
         );
     }
 
-    function validarEmail(url) {
-        $.post(url, function () {
-            alert("success");
-            return 1;
+    function validarEmail(url, result) {
+        $.post(url, function (response) {
+            self.status(response.status);
+            if (self.status() == true) {
+                self.token(response.token);
+                console.log("Token email " + response.token);
+                console.log("True");
+                emailValido(result);
+            }
+            else if (self.status() == false) {
+                alert("E-mail não possui acesso");
+                self.disconnect();
+            }
         })
          .fail(function () {
-             alert("error");
-             return 2;
+             alert("Error");
          });
-        return 3;
         /*$.post(url,  function () {
             console.log(response);
         }, 'json');*/
+        //console.log(self.token());
     }
 
     function emailValido(result) {
         salvarUsuario(result);
         self.userName(result.displayName);
-        console.log(result.serverAuthCode);
         //window.location = "index.html";
         self.pagina('home');
-        self.botaoRemoverDados(false);
         self.botaoDesconectar(true);
     }
+
     self.abrirPorta = function () {
-        alert("Você solicitou abrir a porta");
-        //var url = "http://porta.digitaldesk.com.br/abrirporta?token="+token;
-        //var retornoEmail = validarEmail(url);
-    }           
+        var url = "http://porta.digitaldesk.com.br/abrirporta?token=" + self.token();
+        console.log("Token abrir " + url);
+        //alert(self.token());
+        validarEmail(url, 0);
+    }
 }
