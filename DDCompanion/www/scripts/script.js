03f339d0faeccc1b217ffecb6fc1174874139bc6
Luisa <luisa.rocha@digitaldesk.com.br>
2016-09-14 16:02:43 -0300

    tentativa

--- DDCompanion/www/scripts/script.js
+++ DDCompanion/www/scripts/script.js
@@ -30,6 +30,7 @@
     var x = 0;
     var y = 0;
     var z;
+    var beacons = {};
     var paginaValue;
     var mensagem = {
         portaAberta: 'Porta aberta',
@@ -41,52 +42,51 @@
     }
 
     setTimeout(function () {
-       StatusBar.backgroundColorByHexString("#378613");
-       var GooglePlus = window.plugins.googleplus;
-       var appp = plugins.appPreferences;
-       appp.fetch(function (value) {
-           checarCampos(value);
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "usuario"
-       );
+        StatusBar.backgroundColorByHexString("#378613");
+        var GooglePlus = window.plugins.googleplus;
+        var appp = plugins.appPreferences;
+        appp.fetch(function (value) {
+            checarCampos(value);
+        }, function (err) {
+          //  console.log("Erro " + err);
+        }, "usuario"
+        );
 
-       appp.fetch(function (dadosUsuario) {
-           if (dadosUsuario != null) {
+        appp.fetch(function (dadosUsuario) {
+            if (dadosUsuario != null) {
                 self.nome(dadosUsuario[0]);
                 self.email(dadosUsuario[1]);
                 self.fotoUrl(dadosUsuario[2]);
-          }
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "dadosUsuario"
-       );
+            }
+        }, function (err) {
+           // console.log("Erro " + err);
+        }, "dadosUsuario"
+        );
 
-       appp.fetch(function (valueToken) {
-           self.token(valueToken);
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "token"
-       );
-   }, 1500);
+        appp.fetch(function (valueToken) {
+            self.token(valueToken);
+        }, function (err) {
+          //  console.log("Erro " + err);
+        }, "token"
+        );
+    }, 1500);
 
 
-   function checarCampos(value) {
-       self.loader(false);
-       if (value != null) {
-           self.pagina('home');
-           StatusBar.backgroundColorByHexString("#378613");
-           $('.button-collapse').sideNav('show');
-           self.btnLoginDesabilitado(true);
-       }
-       else { y = 1; z = 0; self.desconectar(); }
-       // 
-   }
+    function checarCampos(value) {
+        self.loader(false);
+        if (value != null) {
+            self.pagina('home');
+            StatusBar.backgroundColorByHexString("#378613");
+            $('.button-collapse').sideNav('show');
+            self.btnLoginDesabilitado(true);
+        }
+        else { y = 1; z = 0; self.desconectar(); }
+        // 
+    }
 
-   self.logarGoogle = function () {
+    self.logarGoogle = function () {
         var GooglePlus = window.plugins.googleplus;
         self.btnLoginDesabilitado(true);
-        setTimeout(function () {
             window.plugins.googleplus.login(
                 {
                     'scopes': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
@@ -101,39 +101,38 @@
                     validarUsuario(url, result, paginaValue);
                 },
                 function (msg) {
-                    console.log(msg);
+                    //console.log(msg);
                     self.btnLoginDesabilitado(false);
-                    alert(mensagem.erroGoogle);
+                    alert(msg);
                 }
             );
-        }, 100);
-   }
+    }
 
-   self.chamarLoginUsuario = function () {
-       self.pagina('loginUsuario');
-       StatusBar.backgroundColorByHexString("#378613");
-   }
+    self.chamarLoginUsuario = function () {
+        self.pagina('loginUsuario');
+        StatusBar.backgroundColorByHexString("#378613");
+    }
 
-   self.voltarTelaLogin = function () {
-       self.pagina('login');
-       StatusBar.backgroundColorByHexString("#378613");
-       self.limparUsuario();
-       self.limparSenha();
-   }
+    self.voltarTelaLogin = function () {
+        self.pagina('login');
+        StatusBar.backgroundColorByHexString("#378613");
+        self.limparUsuario();
+        self.limparSenha();
+    }
 
-   self.logarUsuario = function () {
-       if (self.usuario() == "" || self.senha() == "") { alert(mensagem.campoVazio); }
-       else if (self.usuario() == "" && self.senha() == "") { alert(mensagem.campoVazio); }
-       if (self.usuario() != "" && self.senha() != "") {
-           var url = 'http://porta.digitaldesk.com.br/autenticar/usuario?key=' + self.senha() + '&user=' + self.usuario();
-           paginaValue = self.pagina();
-           self.loader(true);
-           self.pagina('loader');
-           validarUsuario(url, 0, paginaValue);
-       }
-   }
+    self.logarUsuario = function () {
+        if (self.usuario() == "" || self.senha() == "") { alert(mensagem.campoVazio); }
+        else if (self.usuario() == "" && self.senha() == "") { alert(mensagem.campoVazio); }
+        if (self.usuario() != "" && self.senha() != "") {
+            var url = 'http://porta.digitaldesk.com.br/autenticar/usuario?key=' + self.senha() + '&user=' + self.usuario();
+            paginaValue = self.pagina();
+            self.loader(true);
+            self.pagina('loader');
+            validarUsuario(url, 0, paginaValue);
+        }
+    }
 
-   function validarUsuario(url, result, paginaValue) {
+    function validarUsuario(url, result, paginaValue) {
         $.post(url, function (response) {
             self.status(response.status);
             if (self.status() == true) {
@@ -158,71 +157,72 @@
        .fail(function () {
            removerDesativado(mensagem.erro);
        });
-   }
+    }
 
-   function acessoNegado(texto, pagina) {
-       self.loader(false);
-       if (pagina == 'login') {  self.pagina('login'); }
-       else { self.limparSenha(); self.limparUsuario(); self.pagina('loginUsuario');}
-       alert(texto);
-       y = 1;
-       self.desconectar();
-       self.btnLoginDesabilitado(false);
-   }
+    function acessoNegado(texto, pagina) {
+        self.loader(false);
+        if (pagina == 'login') {  self.pagina('login'); }
+        else { self.limparSenha(); self.limparUsuario(); self.pagina('loginUsuario');}
+        alert(texto);
+        y = 1;
+        z = 1;
+        self.desconectar();
+        self.btnLoginDesabilitado(false);
+    }
 
-   function acessoPermitido(result) {
-       self.loader(false);
-       self.pagina('home');
-       self.limparUsuario();
-       self.limparSenha();
-       StatusBar.backgroundColorByHexString("#378613");
-       $('.button-collapse').sideNav('show');
-       self.btnDesconectarDesabilitado(true);
-       dadosUsuario = [result.displayName, result.email, result.imageUrl];
-       salvarUsuario(result);
-       self.nome(dadosUsuario[0]);
-       self.email(dadosUsuario[1]);
-       self.fotoUrl(dadosUsuario[2]);
-       y = 0;
-   }
+    function acessoPermitido(result) {
+        self.loader(false);
+        self.pagina('home');
+        self.limparUsuario();
+        self.limparSenha();
+        StatusBar.backgroundColorByHexString("#378613");
+        $('.button-collapse').sideNav('show');
+        self.btnDesconectarDesabilitado(true);
+        dadosUsuario = [result.displayName, result.email, result.imageUrl];
+        salvarUsuario(result);
+        self.nome(dadosUsuario[0]);
+        self.email(dadosUsuario[1]);
+        self.fotoUrl(dadosUsuario[2]);
+        y = 0;
+    }
 
-   function salvarUsuario(result) {
-       var appp = plugins.appPreferences;
-       appp.store(function (value) {
-           console.log("Dados salvos:  " + value);
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "usuario", result.displayName);
+    function salvarUsuario(result) {
+        var appp = plugins.appPreferences;
+        appp.store(function (value) {
+            //console.log("Dados salvos:  " + value);
+        }, function (err) {
+           // console.log("Erro " + err);
+        }, "usuario", result.displayName);
 
-       appp.store(function (dadosUsuario) {
-           console.log("Dados salvos:  " + dadosUsuario);
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "dadosUsuario", dadosUsuario);
+        appp.store(function (dadosUsuario) {
+         //   console.log("Dados salvos:  " + dadosUsuario);
+        }, function (err) {
+         //   console.log("Erro " + err);
+        }, "dadosUsuario", dadosUsuario);
 
 
-       appp.store(function (valueToken) {
-           console.log("Token salvo: " + valueToken);
-       }, function (err) {
-           console.log("Erro " + err);
-       }, "token", self.token());
-   }
+        appp.store(function (valueToken) {
+         //   console.log("Token salvo: " + valueToken);
+        }, function (err) {
+          //  console.log("Erro " + err);
+        }, "token", self.token());
+    }
 
-   self.desconectar = function () {
-       var GooglePlus = window.plugins.googleplus;
-       if (y == 0) { self.loader(true); self.pagina('loader'); }
-       self.btnDesconectarDesabilitado(false);
-       self.btnLoginDesabilitado(false);
-       window.setTimeout(function () {
-           trySilentLogin();
-           window.plugins.googleplus.disconnect(
-                function (result) {
-                    removerDados();
-                }, function (msg) {
-                    deslogar();
-                });
-       }, 100);       
-   }
+    self.desconectar = function () {
+        var GooglePlus = window.plugins.googleplus;
+        if (y == 0) { self.loader(true); self.pagina('loader'); }
+        self.btnDesconectarDesabilitado(false);
+        self.btnLoginDesabilitado(false);
+        window.setTimeout(function () {
+            trySilentLogin();
+            window.plugins.googleplus.disconnect(
+                 function (result) {
+                     removerDados();
+                 }, function (msg) {
+                     deslogar();
+                 });
+        }, 100);       
+    }
 
     function trySilentLogin() {
         var GooglePlus = window.plugins.googleplus;
@@ -330,20 +330,4 @@
         self.senha('');
     }
 
-    /*self.testeBeacons = function () {
-        self.pagina('testeBeacons');
-        self.caixaGoogle(false);
-        evothings.eddystone.startScan(foundBeacon, scanError);
-    }
-
-    function foundBeacon(beacon) {
-        // Note that beacon.url will be null until the URL
-        // has been received. Also note that not all Eddystone
-        // beacons broadcast URLs, they may send UIDs only.
-        console.log('Found beacon: ' + beacon.url)
-    }
-
-    function scanError(error) {
-        console.log('Eddystone scan error: ' + error)
-    }*/
 }
